f252c836b8ac6aa46fc5098b4ca3ccff63b53e27
Dor Liberman <Dorlib318@gmail.com>
2022-05-01 16:28:51 +0300

    all movies and all directors updates

--- frontend/react/imdb/src/pages/AllMovies.js
+++ frontend/react/imdb/src/pages/AllMovies.js
@@ -4,6 +4,13 @@ import MenuItem from "@mui/material/MenuItem";
 import {Link} from "react-router-dom";
 import NewMovieForm from "../components/movies/NewMovieForm";
 import App from "../App";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import classes from "./top10.module.css";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
 
 function AllMoviesPage(props) {
     const [loadedMovies, setLoadedMovies] = useState([]);
@@ -13,6 +20,11 @@ function AllMoviesPage(props) {
                 rank
                 id
                 title
+                image
+                description
+                director {
+                    name
+                }
             }
         }
     `;
@@ -23,11 +35,38 @@ function AllMoviesPage(props) {
         let loaded
         //let movieId = data["movies"]["id"]
 
-        loaded = data.movies.map(( {title, rank, id}) => (
-            <div key={id}>
-                <p style={{color: "yellow"}}>
-                    <MenuItem style={{fontSize: "x-large"}}><Link to={"/moviePage/" + id} style={{color: "yellow"}} >{title}</Link>:{rank}</MenuItem>
-                </p>
+        loaded = data.movies.map(( {title, rank, id, image, description, director}) => (
+            // <div key={id}>
+            //     <p style={{color: "yellow"}}>
+            //         <MenuItem style={{fontSize: "x-large"}}><Link to={"/moviePage/" + id} style={{color: "yellow"}} >{title}</Link>:{rank}</MenuItem>
+            //     </p>
+            // </div>
+            <div>
+                <Card sx={{maxWidth: 600}} style={{backgroundColor: "#cc2062", marginBottom: "3cm"}} key={id}>
+                    <CardMedia
+                        component="img"
+                        alt="movie image"
+                        height="300"
+                        src={image || 'https://pharem-project.eu/wp-content/themes/consultix/images/no-image-found-360x250.png'}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
+                                    <Link to={"/moviePage/" + id}  style={{color: "yellow" }}  > {title}: {rank} / 100</Link>
+                            </p>
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" style={{fontSize: "large", fontWeight: "bolder"}}>
+                            Description : {description}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" style={{fontSize: "large", fontWeight: "bolder"}}>
+                            Directed By: {director.name}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="large">Share</Button>
+                        <Link to={"/moviePage/" + id} style={{textDecoration: "none"}}><Button size="large">Go To Movie's Page</Button></Link>
+                    </CardActions>
+                </Card>
             </div>
         ));
 
