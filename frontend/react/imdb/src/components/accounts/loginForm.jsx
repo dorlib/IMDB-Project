fc23a027349586ab8178fbe02fa9b1e70395883a
Dor Liberman <Dorlib318@gmail.com>
2022-06-12 21:03:52 +0300

    cookie visible on client side

--- frontend/react/imdb/src/components/accounts/loginForm.jsx
+++ frontend/react/imdb/src/components/accounts/loginForm.jsx
@@ -1,85 +1,113 @@
 import React, {useContext} from "react";
-import {BoldLink, BoxContainer, FormContainer, MutedLink, SubmitButton,} from "./common";
 import {Marginer} from "../marginer";
 import {AccountContext} from "./accountContext";
 import {useState} from "react";
 import Card from "../ui/Card";
 import {styled} from "@mui/material/styles";
-import {gql, useLazyQuery, useMutation, useQuery} from "@apollo/client";
 
-import classes from "./loginForm.module.css";
+import classes from "./LoginForm.module.css";
+import {BoldLink, BoxContainer, FormContainer, MutedLink, SubmitButton,} from "./common";
+import {Typography} from "@mui/material";
+import styledComponentsBrowserEsm from "styled-components/dist/styled-components.browser.esm";
+
 
 export function LoginForm() {
     const {switchToSignup} = useContext(AccountContext);
-    const LOGIN_USER = gql`
-        query LoginUser ($nickname: String!, $password: String!, $email: String!) {
-            loginUser(nickname: $nickname, password: $password, email: $email) {
-                id
-                firstname
-            }
-        }
-    `;
+    const {switchToReset} = useContext(AccountContext);
+
 
     const [givenPassword, setPassword] = useState('')
     const [givenNickname, setNickname] = useState('')
-    const [givenEmail, setEmail] = useState('')
-    const Input = styled("input")({
-        display: "none",
-    });
+    // const [givenEmail, setEmail] = useState('')
 
-    const [loginUser, {loading, error}] = useLazyQuery(LOGIN_USER,
-        {
-            variables: {
-                nickname: givenNickname,
-                password: givenPassword,
-                email: givenEmail,
-            }, onCompleted: function (data) {
-                if (data && data["loginUser"]) {
-                    return (
-                        console.log(data),
-                            <p>Welcome Back {data["loginUser"]["0"]["firstname"]}</p>,
-                            window.location.replace("/userPage/" + data["loginUser"]["0"]["id"])
-                    )
+    const [spinner, setSpinner] = useState(false);
+    const [loginError, setLoginError] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [userName, setUserName] = useState('')
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const userData = {
+            givenPassword,
+            givenNickname,
+            // givenEmail
+        };
+
+        setSpinner(true);
+        setLoginError(false);
+        setSuccess(false);
+
+        fetch('http://localhost:8081/loginForm', {
+            method: 'post',
+            credentials: 'include',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(userData)
+        })
+            .then(response => response.json())
+            .catch((err) => {
+                console.error('error:', err)
+            })
+            .then((data) => {
+                if (data) {
+                    console.log('login successfully')
+                    console.log(data["Cookie"])
+                    setUserName(data["FirstName"])
+                    setSpinner(false);
+                    setSuccess(true);
+                    return setTimeout( () => window.location.replace("/userPage/" + JSON.stringify(data["ID"])), 1000)
                 } else {
-                    return (
-                        console.log(data),
-                            console.log("login failed"),
-                            window.location.reload()
-                    )
+                    console.log('login not successful')
+                    setSpinner(false);
+                    setLoginError(true)
+                    return setTimeout(() => window.location.reload(),2000);
                 }
-            },
+            })
+            // .then(        NEED TO RETURN ANOTHER FETCH NOT TO THEN ANOTHER FETCH!!
+            //     fetch('http://localhost:8081/user', {
+            //         method: 'get',
+            //         headers: {"Content-Type": "application/json"},
+            //     }).then()
+            // )
+    }
+
+        const Input = styled("input")({
+            display: "none",
         });
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Login Failed.. Please Try Again</p>;
-
-    return (
-        <BoxContainer>
-            <form className={classes.form} action="loginForm.jsx" method="POST">
-                <Card>
-                    <div className={classes.control}>
-                        <label htmlFor="nickName">Enter Your Nickname</label>
-                        <input type="text" id="nickname" name="nickname" required
-                               onChange={event => setNickname(event.target.value)} autoComplete="username"/>
-                    </div>
-                    <div className={classes.ctrl}>
-                        <label htmlFor="password">Enter Your password (8 characters minimum</label>
-                        <input type="password" id="password" name="password" minLength="8" required
-                               onChange={event => setPassword(event.target.value)} autoComplete="new-password"/>
-                    </div>
-                </Card>
-            </form>
-            <SubmitButton type="submit" /*onClick={loginUser}*/>Log In!</SubmitButton>
-            <Marginer direction="vertical" margin={10}/>
-            <MutedLink href="#">Forget your password?</MutedLink>
-            <Marginer direction="vertical" margin="0.3em"/>
-            <Marginer direction="vertical" margin="1em"/>
-            <MutedLink href="#">
-                Don't have an account?{" "}
-                <BoldLink href="#" onClick={switchToSignup}>
-                    Sign Up
-                </BoldLink>
-            </MutedLink>
-        </BoxContainer>
-    );
-}
+        return (
+            <BoxContainer>
+                <form className={classes.form} onSubmit={handleLogin}>
+                    <Card>
+                        <div className={classes.control}>
+                            <label htmlFor="nickname">Enter Your Nickname</label>
+                            <input type="text" id="nickname" name="nickname" required
+                                   onChange={event => setNickname(event.target.value)} autoComplete="username"/>
+                        </div>
+                        <div className={classes.ctrl}>
+                            <label htmlFor="password">Enter Your password (8 characters minimum</label>
+                            <input type="password" id="password" name="password" minLength="8" required
+                                   onChange={event => setPassword(event.target.value)} autoComplete="new-password"/>
+                        </div>
+                    </Card>
+                    <Typography className={classes.err}>{loginError? 'Login was not successful... please try again': null}</Typography>
+                    <Typography className={classes.success}>{success? 'Its Nice Seeing You Again '+ userName + ' !': null}</Typography>
+                    <Marginer direction="vertical" margin={45}/>
+                    <SubmitButton type="submit" value="submit" style={{display: "flex", marginLeft: "1.9cm"}}>{spinner? 'loading...' : 'Log In!'}</SubmitButton>
+                </form>
+                <MutedLink href="#">Forget your password?
+                    <BoldLink href="#" onClick={switchToReset}>
+                        click here to reset
+                    </BoldLink>
+                </MutedLink>
+                <Marginer direction="vertical" margin="0.3em"/>
+                <Marginer direction="vertical" margin="1em"/>
+                <MutedLink href="#">
+                    Don't have an account?{" "}
+                    <BoldLink href="#" onClick={switchToSignup}>
+                        Sign Up
+                    </BoldLink>
+                </MutedLink>
+            </BoxContainer>
+        );
+    }
