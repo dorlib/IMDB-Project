c31bb9297e2e0bb35f0d47edab1177d9dbd2e4c1
Dor Liberman <Dorlib318@gmail.com>
2022-06-16 21:59:41 +0300

    not logged in user cannot write reviews

--- frontend/react/imdb/src/components/layout/Layout.js
+++ frontend/react/imdb/src/components/layout/Layout.js
@@ -4,28 +4,8 @@ import React, {useEffect, useState} from "react";
 import SearchBar from "./search-bar";
 
 function Layout(props) {
-    const [userFirstName, setUserFirstName] = useState('Guest')
-    const [userId, setUserId] = useState(0)
-
-    useEffect(() => {
-        (
-            async () => {
-                await fetch("http://localhost:8081/user", {
-                    method: 'get',
-                    headers: {'Content-Type': 'application/json'},
-                    credentials: 'include',
-                }).then(response => response.json())
-                    .catch((err) => {
-                        console.error('error:', err)
-                    })
-                    .then((data) => {
-                        console.log(data)
-                        setUserId(data["0"]["id"])
-                        setUserFirstName(data["0"]["firstname"])
-                    });
-            }
-        )();
-    });
+    let userFirstName = props.username
+    let userId = props.userId
 
     return (
         <div>
