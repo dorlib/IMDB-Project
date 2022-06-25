af828829665ab8bdf860a0b807ae15d319087135
Dor Liberman <Dorlib318@gmail.com>
2022-06-25 21:47:38 +0300

    user can see his own favorites

--- frontend/react/imdb/src/components/movies/MovieList.js
+++ frontend/react/imdb/src/components/movies/MovieList.js
@@ -15,25 +15,25 @@ function MovieList(props) {
 
   return (
     <ul className={classes.list}>
-      {movies.map((movie) => (
+      {movies.favoritesOfUser.map(( {id, movieTitle, movieImage} ) => (
           <div>
-            <Card sx={{maxWidth: 600}} style={{backgroundColor: "#cc2062", marginBottom: "3cm", borderRadius: "15px"}} key={movies["favoritesOfUser"]["0"]["id"]}>
+            <Card sx={{maxWidth: 400}} style={{backgroundColor: "#cc2062", marginBottom: "3cm", borderRadius: "15px"}} key={id}>
               <CardMedia
                   component="img"
                   alt="movie image"
-                  height="300"
+                  height="400"
                   src={props.image || 'https://pharem-project.eu/wp-content/themes/consultix/images/no-image-found-360x250.png'}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                    <Link to={"/moviePage/" + movies["favoritesOfUser"]["0"]["id"]} style={{color: "yellow" }}> {movies["favoritesOfUser"]["0"]["movieTitle"]}</Link>
+                    <Link to={"/moviePage/" + id} style={{color: "yellow" }}> {movieTitle}</Link>
                   </p>
                 </Typography>
               </CardContent>
               <CardActions>
                 <Button size="large">Share</Button>
-                <Link to={"/moviePage/" + movies["favoritesOfUser"]["0"]["id"]} style={{textDecoration: "none"}}><Button size="large">Go To Movie's Page</Button></Link>
+                <Link to={"/moviePage/" + id} style={{textDecoration: "none"}}><Button size="large">Go To Movie's Page</Button></Link>
               </CardActions>
             </Card>
           </div>
