ec0dbb1fff25c3504990c9b6011e7fed90ad05e3
Dor Liberman <Dorlib318@gmail.com>
2022-06-25 23:03:16 +0300

    styling added to my favorites page

--- frontend/react/imdb/src/components/movies/MovieList.js
+++ frontend/react/imdb/src/components/movies/MovieList.js
@@ -8,32 +8,32 @@ import {Link} from "react-router-dom";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import React from "react";
-import classes from "../../pages/AllMovies"
+import classes from "./MovieList.module.css"
 
 function MovieList(props) {
   let movies = props.movies
 
   return (
     <ul className={classes.list}>
-      {movies.favoritesOfUser.map(( {id, movieTitle, movieImage} ) => (
+      {movies.favoritesOfUser.map(( {movieID, movieTitle, movieImage} ) => (
           <div>
-            <Card sx={{maxWidth: 400}} style={{backgroundColor: "#cc2062", marginBottom: "3cm", borderRadius: "15px"}} key={id}>
+            <Card sx={{maxWidth: 500}} style={{backgroundColor: "#cc2062", borderRadius: "15px", width: "400px", display: "inline-block"}} key={movieID}>
               <CardMedia
                   component="img"
                   alt="movie image"
                   height="400"
-                  src={props.image || 'https://pharem-project.eu/wp-content/themes/consultix/images/no-image-found-360x250.png'}
+                  src={movieImage || 'https://pharem-project.eu/wp-content/themes/consultix/images/no-image-found-360x250.png'}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                    <Link to={"/moviePage/" + id} style={{color: "yellow" }}> {movieTitle}</Link>
+                    <Link to={"/moviePage/" + movieID} className={classes.movieTitle} > {movieTitle}</Link>
                   </p>
                 </Typography>
               </CardContent>
               <CardActions>
                 <Button size="large">Share</Button>
-                <Link to={"/moviePage/" + id} style={{textDecoration: "none"}}><Button size="large">Go To Movie's Page</Button></Link>
+                <Link to={"/moviePage/" + movieID} style={{textDecoration: "none"}}><Button size="large">Go To Movie's Page</Button></Link>
               </CardActions>
             </Card>
           </div>
