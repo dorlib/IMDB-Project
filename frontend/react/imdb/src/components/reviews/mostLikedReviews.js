6e5a18f978207fee7897ee4ecb30233756b29b6e
Dor Liberman <Dorlib318@gmail.com>
2022-07-23 22:42:01 +0300

    most liked reviews of user on progeress on client side

--- frontend/react/imdb/src/components/reviews/mostLikedReviews.js
+++ frontend/react/imdb/src/components/reviews/mostLikedReviews.js
@@ -3,7 +3,7 @@ import {gql, useQuery} from "@apollo/client";
 import {Link} from "react-router-dom";
 
 import Button from "@mui/material/Button";
-import classes from "./last5Added.module.css"
+import classes from "./mostLikedReviews.module.css"
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -44,6 +44,12 @@ function MostLikedReviews() {
         }
     }
 
+    console.log(data)
+
+    let numOfReviews = data.mostLikedReviews.length
+
+    console.log(numOfReviews)
+
     const {loading, error, data} = useQuery(MOST_LIKED_REVIEWS)
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error}</p>;
@@ -60,22 +66,18 @@ function MostLikedReviews() {
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                            {data.mostLikedReviews["0"]["topic"]} : {data.mostLikedReviews["0"]["rank"]} / 100
+                            <Link to={"/moviePage/" + data.mostLikedReviews["0"]["movie"]["id"]}>{data.mostLikedReviews["0"]["movie"]["title"]}</Link>
                         </p>
                     </Typography>
-                    {{data.mostLikedReviews["0"]["rank"]} !== null ? <span>{data.mostLikedReviews["0"]["rank"]} / 100}</span>: null}
-                    {{data.mostLikedReviews["0"]["topic"]} !== "" ? <span>data.mostLikedReviews["0"]["topic"]}</span> : null}
-                    <Typography>
-                    </Typography>
-                    {data.mostLikedReviews["0"]["text"] !== "" ? <Typography>
-                        {data.mostLikedReviews["0"]["text"]}
-                    </Typography> : null}
+                    {data.mostLikedReviews["0"]["topic"] !== "" ? <span>{data.mostLikedReviews["0"]["topic"]}</span> : null}
+                    {data.mostLikedReviews["0"]["rank"] !== null ? <span>{data.mostLikedReviews["0"]["rank"] / 100}</span>: null}
+                    {data.mostLikedReviews["0"]["text"] !== "" ? <Typography>{data.mostLikedReviews["0"]["text"]} </Typography> : null}
                     <div>{numOfSet + 1 / 5}</div>
                 </CardContent>
             </Card>
         </div>
     )
-    let card2 = (
+    let review2 = (
         <div>
             <Card sx={{maxWidth: 600}} style={{
                 backgroundColor: "#cc2062",
@@ -84,32 +86,21 @@ function MostLikedReviews() {
                 marginTop: "-31rem",
                 borderRadius: "100px"
             }}>
-                <CardMedia component="img" alt="movie image" height="300" src={data.last5Added["1"]["image"]}
-                />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                            {data.last5Added["1"]["title"]} : {data.last5Added["1"]["rank"]} / 100
+                            <Link to={"/moviePage/" + data.mostLikedReviews["1"]["movie"]["id"]}>{data.mostLikedReviews["1"]["movie"]["title"]}</Link>
                         </p>
-                        <Typography className={classes.genre}>Genre : {data.last5Added["1"]["genre"]}</Typography>
                     </Typography>
-                    <div  style={{textAlign: "center" , marginTop: "0.2cm"}} >
-                        <span className={classes.emptyDot}></span>
-                        <span className={classes.fullDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                    </div>
+                    {data.mostLikedReviews["1"]["topic"] !== "" ? <span>{data.mostLikedReviews["1"]["topic"]}</span> : null}
+                    {data.mostLikedReviews["1"]["rank"] >= 0 ? <span>{data.mostLikedReviews["1"]["rank"]} / 100}</span>: null}
+                    {data.mostLikedReviews["1"]["text"] !== "" ? <Typography>{data.mostLikedReviews["1"]["text"]} </Typography> : null}
+                    <div>{numOfSet + 1 / 5}</div>
                 </CardContent>
-                <CardActions className={classes.buttons}>
-                    <Button size="large">Share</Button>
-                    <Link to={"/moviePage/" + data.last5Added["1"]["id"]} style={{textDecoration: "none"}}><Button
-                        size="large">Go To Movie's Page</Button></Link>
-                </CardActions>
             </Card>
         </div>
     )
-    let card3 = (
+    let review3 = (
         <div>
             <Card sx={{maxWidth: 600}} style={{
                 backgroundColor: "#cc2062",
@@ -118,32 +109,21 @@ function MostLikedReviews() {
                 marginTop: "-31rem",
                 borderRadius: "100px"
             }}>
-                <CardMedia component="img" alt="movie image" height="300" src={data.last5Added["2"]["image"]}
-                />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                            {data.last5Added["2"]["title"]} : {data.last5Added["2"]["rank"]} / 100
+                            <Link to={"/moviePage/" + data.mostLikedReviews["2"]["movie"]["id"]}>{data.mostLikedReviews["2"]["movie"]["title"]}</Link>
                         </p>
-                        <Typography className={classes.genre}>Genre : {data.last5Added["2"]["genre"]}</Typography>
                     </Typography>
-                    <div  style={{textAlign: "center" , marginTop: "0.2cm"}} >
-                        <span className={classes.emptyDot}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.fullDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                    </div>
+                    {data.mostLikedReviews["2"]["topic"] !== "" ? <span>{data.mostLikedReviews["2"]["topic"]}</span> : null}
+                    {data.mostLikedReviews["2"]["rank"] >= 0 ? <span>{data.mostLikedReviews["2"]["rank"]} / 100}</span>: null}
+                    {data.mostLikedReviews["2"]["text"] !== "" ? <Typography>{data.mostLikedReviews["2"]["text"]} </Typography> : null}
+                    <div>{numOfSet + 1 / 5}</div>
                 </CardContent>
-                <CardActions className={classes.buttons}>
-                    <Button size="large">Share</Button>
-                    <Link to={"/moviePage/" + data.last5Added["2"]["id"]} style={{textDecoration: "none"}}><Button
-                        size="large">Go To Movie's Page</Button></Link>
-                </CardActions>
             </Card>
         </div>
     )
-    let card4 = (
+    let review4 = (
         <div>
             <Card sx={{maxWidth: 600}} style={{
                 backgroundColor: "#cc2062",
@@ -152,32 +132,21 @@ function MostLikedReviews() {
                 marginTop: "-31rem",
                 borderRadius: "100px"
             }}>
-                <CardMedia component="img" alt="movie image" height="300" src={data.last5Added["3"]["image"]}
-                />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                            {data.last5Added["3"]["title"]} : {data.last5Added["3"]["rank"]} / 100
+                            <Link to={"/moviePage/" + data.mostLikedReviews["3"]["movie"]["id"]}>{data.mostLikedReviews["3"]["movie"]["title"]}</Link>
                         </p>
-                        <Typography className={classes.genre}>Genre : {data.last5Added["3"]["genre"]}</Typography>
                     </Typography>
-                    <div  style={{textAlign: "center" , marginTop: "0.2cm"}} >
-                        <span className={classes.emptyDot}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.fullDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                    </div>
+                    {data.mostLikedReviews["3"]["topic"] !== "" ? <span>{data.mostLikedReviews["3"]["topic"]}</span> : null}
+                    {data.mostLikedReviews["3"]["rank"] >= 0 ? <span>{data.mostLikedReviews["3"]["rank"]} / 100}</span>: null}
+                    {data.mostLikedReviews["3"]["text"] !== "" ? <Typography>{data.mostLikedReviews["3"]["text"]} </Typography> : null}
+                    <div>{numOfSet + 1 / 5}</div>
                 </CardContent>
-                <CardActions className={classes.buttons}>
-                    <Button size="large">Share</Button>
-                    <Link to={"/moviePage/" + data.last5Added["3"]["id"]} style={{textDecoration: "none"}}><Button
-                        size="large">Go To Movie's Page</Button></Link>
-                </CardActions>
             </Card>
         </div>
     )
-    let card5 = (
+    let review5 = (
         <div>
             <Card sx={{maxWidth: 600}} style={{
                 backgroundColor: "#cc2062",
@@ -186,78 +155,32 @@ function MostLikedReviews() {
                 marginTop: "-31rem",
                 borderRadius: "100px"
             }}>
-                <CardMedia component="img" alt="movie image" height="300" src={data.last5Added["4"]["image"]}
-                />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <p style={{color: "yellow", fontSize: "xx-large"}} className={classes.movie}>
-                            {data.last5Added["4"]["title"]} : {data.last5Added["4"]["rank"]} / 100
+                            <Link to={"/moviePage/" + data.mostLikedReviews["4"]["movie"]["id"]}>{data.mostLikedReviews["4"]["movie"]["title"]}</Link>
                         </p>
-                        <Typography className={classes.genre}>Genre : {data.last5Added["4"]["genre"]}</Typography>
                     </Typography>
-                    <div  style={{textAlign: "center" , marginTop: "0.2cm" }} >
-                        <span className={classes.emptyDot} ></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.emptyDot} style={{marginLeft: "0.1cm"}}></span>
-                        <span className={classes.fullDot} style={{marginLeft: "0.1cm"}}></span>
-                    </div>
+                    {data.mostLikedReviews["4"]["topic"] !== "" ? <span>{data.mostLikedReviews["4"]["topic"]}</span> : null}
+                    {data.mostLikedReviews["4"]["rank"] > 0 ? <span>{data.mostLikedReviews["4"]["rank"]} / 100}</span>: null}
+                    {data.mostLikedReviews["4"]["text"] !== "" ? <Typography>{data.mostLikedReviews["4"]["text"]} </Typography> : null}
+                    <div>{numOfSet + 1 / 5}</div>
                 </CardContent>
-                <CardActions className={classes.buttons}>
-                    <Button size="large">Share</Button>
-                    <Link to={"/moviePage/" + data.last5Added["4"]["id"]} style={{textDecoration: "none"}}><Button
-                        size="large">Go To Movie's Page</Button></Link>
-                </CardActions>
             </Card>
         </div>
     )
 
-    let cards = [card1, card2, card3, card4, card5]
-
-    const forwardInput = () => {
-        if (currentIndex === 4) {
-            setCurrentIndex(0)
-        } else {
-            setCurrentIndex(currentIndex + 1)
-        }
-    }
-
-    const backwardInput = () => {
-        if (currentIndex === 0) {
-            setCurrentIndex(4)
-        } else {
-            setCurrentIndex(currentIndex - 1)
-        }
-    }
+    let reviews = [review1, review2, review3, review4, review5]
 
     return (
         <div>
-            <ArrowForwardIosIcon className={classes.forward} style={{fontSize:"xxx-large"}} id={"Forward"} onClick={forwardInput}/>
-            <ArrowBackIosNewIcon className={classes.backward} style={{fontSize:"xxx-large"}} id={"Backward"} onClick={backwardInput}/>
-            <Swapper/>
+            <ArrowForwardIosIcon className={classes.forward} style={{fontSize:"xxx-large"}} id={"Forward"} onClick={handleClickForward}/>
+            <ArrowBackIosNewIcon className={classes.backward} style={{fontSize:"xxx-large"}} id={"Backward"} onClick={handleClickBackward}/>
+            {reviews[numOfSet]}
         </div>
     )
 }
 
-export default Last5Added;
-
-
-
-
-
-
-
-
-// import React, {useState} from 'react'
-// import {gql, useQuery} from "@apollo/client";
-// import Card from "@mui/material/Card";
-// import CardMedia from "@mui/material/CardMedia";
-// import CardContent from "@mui/material/CardContent";
-// import Typography from "@mui/material/Typography";
-// import classes from "./mostLikedReviews.module.css";
-// import {Link} from "react-router-dom";
-// import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-// import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 //
 // function MostLikedReviews(props) {
 //     const [numOfSet, setNumOfSet] = useState(0)
@@ -322,4 +245,4 @@ export default Last5Added;
 //
 // }
 //
-// export default MostLikedReviews
+export default MostLikedReviews
