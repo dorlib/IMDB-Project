73156d6552b6356c5e0c0af598da8f5c44ca1e92
Dor Liberman <Dorlib318@gmail.com>
2022-08-20 19:20:25 +0300

    achivements on progress

--- frontend/react/imdb/src/components/accounts/show-achievement.js
+++ frontend/react/imdb/src/components/accounts/show-achievement.js
@@ -22,37 +22,22 @@ function ShowAchievement(props) {
 
     let load = (
         <div style={{position: "absolute"}}>
-            {numOfSet === numOfSets || numOfDirectors <= 8 ? null : <ArrowForwardIosIcon className={classes.forward} onClick={handleClickForward} style={{fontSize: "xx-large"}} id={"Forward"}/>}
-            {numOfSet === 0 || numOfDirectors <= 8 ? null : <ArrowBackIosNewIcon className={classes.backward} onClick={handleClickBackward} style={{fontSize: "xx-large"}} id={"Backward"}/>}
-            <ul className={classes.list}>
-                {newData.map(({id, name, profileImage}) => (
-                    <Card sx={{maxWidth: 100}} style={{
-                        backgroundColor: "#cc2062",
-                        borderRadius: "50px",
-                        width: "90px",
-                        display: "inline-block",
-                        marginTop: "0.7cm"
-                    }} key={id}>
-                        <CardMedia
-                            component="img"
-                            alt="movie image"
-                            height="90"
-                            src={profileImage || 'https://hope.be/wp-content/uploads/2015/05/no-user-image.gif'}
-                        />
-                        <CardContent className={classes.card}>
-                            <Typography gutterBottom variant="h5" component="div">
-                                <p style={{color: "yellow"}} className={classes.director}>
-                                    <Link to={"/directorPage/" + id} className={classes.directorName}> {name}</Link>
-                                </p>
-                                <div className={classes.outOf}>{numOfSet + 1  + "/" + (numOfSets+1)}</div>
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                ))}
-            </ul>
+            <Card sx={{maxWidth: 100}} style={{
+                backgroundColor: "#cc2062",
+                borderRadius: "50px",
+                width: "90px",
+                display: "inline-block",
+                marginTop: "0.7cm"
+            }}>
+                <CardMedia
+                    component="img"
+                    alt="movie image"
+                    height="90"
+                    src={image}
+                />
+            </Card>
         </div>
     )
-
 
 
     if (image !== "") {
