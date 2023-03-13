4c31fa80652165ebbd067806de2f05c86261fad9
dor <dorlibrm@gmail.com>
2023-03-13 22:24:21 +0200

    add seeder WIP

--- backend/server/careers/src/main/java/com/careersapp/careers/CareersApplication.java
+++ backend/server/careers/src/main/java/com/careersapp/careers/CareersApplication.java
@@ -1,5 +1,6 @@
 package com.careersapp.careers;
 
+import com.careersapp.careers.model.Position;
 import com.careersapp.careers.repository.IPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,12 +13,25 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class CareersApplication implements CommandLineRunner {
 
     @Autowired
-    IPosition positionRepo;
+    public IPosition positionRepo;
 
     public static void main(String[] args) {
 
+        CareersApplication app = new CareersApplication();
+
         SpringApplication.run(CareersApplication.class, args);
+        app.createPositions();
+
     }
+
+    public void createPositions() {
+        positionRepo.save(new Position(1, "Software Engineer", "backend software engineer", "hybrid", "R&D"));
+        positionRepo.save(new Position(1, "team lead", "team lead of backend software engineers", "hybrid", "R&D"));
+        positionRepo.save(new Position(1, "office manager", "HR and wellness", "On Site", "HR"));
+        positionRepo.save(new Position(1, "data scientist", "data scientist in AI group", "hybrid", "R&D"));
+        positionRepo.save(new Position(1, "IT manager", "manage IT and system", "On Site", "system"));
+    }
+
 
     @Override
     public void run(String... args) throws Exception {
