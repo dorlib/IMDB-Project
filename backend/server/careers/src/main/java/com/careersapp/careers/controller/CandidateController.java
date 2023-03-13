a00bfbafd41ffdfc7227a5ee773b73d0fa4f2743
dor <dorlibrm@gmail.com>
2023-03-13 22:23:40 +0200

    add controllers

---
+++ backend/server/careers/src/main/java/com/careersapp/careers/controller/CandidateController.java
@@ -0,0 +1,17 @@
+package com.careersapp.careers.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/candidate")
+public class CandidateController {
+
+    public void addCandidate() {};
+    public void removePCandidate() {};
+    public void getAllCandidates() {};
+    public void updateCandidate() {};
+    public void getCandidateByFirstName() {};
+    public void getCandidateByLastName() {};
+    public void getCandidateByID() {};
+}
---
+++ backend/server/careers/src/main/java/com/careersapp/careers/controller/PositionController.java
@@ -0,0 +1,19 @@
+package com.careersapp.careers.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/position")
+public class PositionController {
+
+    public void addPosition() {};
+    public void removePosition() {};
+    public void getAllPositions() {};
+    public void updatePosition() {};
+    public void getPositionByDepartment() {};
+    public void getPositionByID() {};
+    public void getPositionByName() {};
+    public void getPositionByModel() {};
+
+}
