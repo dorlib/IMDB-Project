0a414521aa8e1c87a2cfcd335aa6eddb33b3dd33
dor <dorlibrm@gmail.com>
2023-03-13 22:24:07 +0200

    update models

--- backend/server/careers/src/main/java/com/careersapp/careers/model/Candidate.java
+++ backend/server/careers/src/main/java/com/careersapp/careers/model/Candidate.java
@@ -3,7 +3,7 @@ package com.careersapp.careers.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document("candidate")
 public class Candidate {
     @Id
     int candidateID;
@@ -37,5 +37,85 @@ public class Candidate {
         education = education;
         experience = experience;
         positionID = positionID;
+    }
+
+    public int getPositionID() {
+        return positionID;
+    }
+
+    public int getCandidateID() {
+        return candidateID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String[] getEducation() {
+        return education;
+    }
+
+    public String[] getExperience() {
+        return experience;
+    }
+
+    public void setCandidateID(int candidateID) {
+        this.candidateID = candidateID;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setEducation(String[] education) {
+        this.education = education;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setExperience(String[] experience) {
+        this.experience = experience;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPositionID(int positionID) {
+        this.positionID = positionID;
     }
 }
--- backend/server/careers/src/main/java/com/careersapp/careers/model/Position.java
+++ backend/server/careers/src/main/java/com/careersapp/careers/model/Position.java
@@ -3,7 +3,7 @@ package com.careersapp.careers.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document("position")
 public class Position {
     @Id
     int positionID;
@@ -24,5 +24,45 @@ public class Position {
         description = description;
         model = model;
         department = description;
+    }
+
+    public int getPositionID() {
+        return positionID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPositionID(int positionID) {
+        this.positionID = positionID;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
